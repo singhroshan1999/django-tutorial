b9db797814b3ea99fa08f11849b175a0f86aeb85
Roshan Singh <singhroshan1999@gmail.com>
2019-06-11 00:31:20 +0530

    models MTV-paradgim database

--- djangoTutorial/first_app/views.py
+++ djangoTutorial/first_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Webpage,AccessRecord
 
 # Create your views here.
 #####
 def index(request):
-    my_dict = {'insert_me':"hello this is content of template in views.py"}
+    webp = AccessRecord.objects.order_by('date')
+    my_dict = {'acc_rec':webp}
     return render(request,'first_app/index.html',context=my_dict)
